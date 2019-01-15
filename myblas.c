f41d0948f7e0c46c592889cffc5b236d0be059c7
Dr. Alberto Cruz <albert@odin.cs.csubak.edu>
2019-01-15 15:23:13 -0800

    Yep

--- myblas.c
+++ myblas.c
@@ -8,18 +8,3 @@ int dewvm( int N, double *x, double *y, double *result ) {
     return 0;
 }
 
-// DGEMV - Double precision generalized matrix time vector
-// Carries out A * x + y, where A is an N * N matrix, and x and y are N * 1 vectors. 
-int dgemv( int N, double *A, double *x, double *y, double *result ) {
-    for( int i = 0; i < N; i++ ) {
-        // First term
-        double c = 0;
-        for( int j = 0; j < N; j++ )
-            c += A[i + j*N] * x[i];
-
-        // Second term, place directly in *result
-        result[i] = c + y[i];
-    }
-
-    return 0;
-}
--- test_dewvm.c
+++ test_dewvm.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "myblas.h"
 
+#define LENGTH 4
+
 // 0 if x matches y
 int checkArrays( int N, double *x, double *y ) {
     for( int i = 0; i < N; i++ )
@@ -12,10 +14,17 @@ int checkArrays( int N, double *x, double *y ) {
 
 int main( void ) {
     // Test on a small vector of 2x1
-    double array1[4] = { 13.0, 13.0, 13.0, 13.0 };
-    double array2[4] = { 7.0, 7.0, 7.0, 7.0 };
-    double result[4];
-    dewvm( 4, array1, array2, result );
+    double array1[LENGTH] = { 1.0, 2.0, 3.0, 4.0 };
+    double array2[LENGTH] = { 1.0, 2.0, 3.0, 4.0 };
+    double result[LENGTH];
+    dewvm( LENGTH, array1, array2, result );
+
+    double correctResult[LENGTH] = { 1.0, 4.0, 9.0, 16.0 };
+
+    if( !checkArrays( LENGTH, result, correctResult ) )
+       printf( "Works fine!\n" );
+    else
+       printf( "Does not work.\n" ); 
 
     return 0;
 }
